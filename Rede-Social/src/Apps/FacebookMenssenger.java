892fdd259349c11d0c0fade00406650227f80d44
Vitor Cunha Silva <151567991+Vitorcunh@users.noreply.github.com>
2024-05-29 21:24:00 -0300

    Rede Social

---
+++ Rede-Social/src/Apps/FacebookMenssenger.java
@@ -0,0 +1,10 @@
+package Apps;
+public class FacebookMenssenger extends ServicoMensagemInstantanea {
+  public void enviarMensagem() {
+		validarConectadoInternet();
+		System.out.println("Enviando mensagem pelo Facebook Messenger");
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Facebook Messenger");
+	}
+}
---
+++ Rede-Social/src/ComputadorPedrinho.java
@@ -0,0 +1,29 @@
+import Apps.FacebookMenssenger;
+import Apps.MSNMessenger;
+import Apps.ServicoMensagemInstantanea;
+import Apps.Telegram;
+
+public class ComputadorPedrinho {
+      public static void main(String[] args) {
+        
+        ServicoMensagemInstantanea smi = null;
+        
+        /*
+            NÃO SE SABE QUAL APP 
+            MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+         */
+        String appEscolhido="???"; 
+        
+        if(appEscolhido.equals("msn"))
+          smi = new MSNMessenger();
+        else if(appEscolhido.equals("fbm"))
+          smi = new FacebookMenssenger();
+        else if(appEscolhido.equals("tlg"))
+          smi = new Telegram();
+        
+          
+        smi.enviarMensagem();
+        smi.receberMensagem();
+      }
+    }
+
